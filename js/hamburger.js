ee5e6b655a47229b025e90f9ab8a48649fdfd4e6
gintocg <ginto.cs22@jecc.ac.in>
2025-06-04 12:39:41 +0530

    6

---
+++ js/hamburger.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
