3296845c279bc4a63667fa8efe05734bf74a89e8
gintocg <ginto.cs22@jecc.ac.in>
2025-06-12 01:57:54 +0530

    Update testimonialCards.js

--- js/testimonialCards.js
+++ js/testimonialCards.js
@@ -1,32 +1,29 @@
-<script>
-  document.addEventListener("DOMContentLoaded", () => {
-    let index = 1;
-    const cards = document.querySelectorAll('.testimonial-cards .card');
+document.addEventListener("DOMContentLoaded", () => {
+  let index = 1;
+  const cards = document.querySelectorAll('.testimonial-cards .card');
 
-    function updateCarousel() {
-      cards.forEach((card, i) => {
-        card.classList.remove('active');
-        if (i === index) {
-          card.classList.add('active');
-        }
-      });
-    }
+  function updateCarousel() {
+    cards.forEach((card, i) => {
+      card.classList.remove('active');
+      if (i === index) {
+        card.classList.add('active');
+      }
+    });
+  }
 
-    function nextSlide() {
-      index = (index + 1) % cards.length;
-      updateCarousel();
-    }
-
-    function prevSlide() {
-      index = (index - 1 + cards.length) % cards.length;
-      updateCarousel();
-    }
-
-    // Attach functions to global scope if using inline HTML onclick
-    window.nextSlide = nextSlide;
-    window.prevSlide = prevSlide;
-
+  function nextSlide() {
+    index = (index + 1) % cards.length;
     updateCarousel();
-    setInterval(nextSlide, 4000);
-  });
-</script>
+  }
+
+  function prevSlide() {
+    index = (index - 1 + cards.length) % cards.length;
+    updateCarousel();
+  }
+
+  window.nextSlide = nextSlide;
+  window.prevSlide = prevSlide;
+
+  updateCarousel();
+  setInterval(nextSlide, 4000);
+});
