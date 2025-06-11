afbcf460d88fb743daee9abef93debd1e1a178c2
gintocg <ginto.cs22@jecc.ac.in>
2025-06-12 01:38:33 +0530

    Update testimonial-navigation.js

--- js/testimonial-navigation.js
+++ js/testimonial-navigation.js
@@ -1,25 +1,32 @@
 <script>
-  let index = 1;
-  const cards = document.querySelectorAll('.testimonial-cards .card');
+  document.addEventListener("DOMContentLoaded", () => {
+    let index = 1;
+    const cards = document.querySelectorAll('.testimonial-cards .card');
 
-  function updateCarousel() {
-    cards.forEach((card, i) => {
-      card.classList.remove('active');
-      if (i === index) {
-        card.classList.add('active');
-      }
-    });
-  }
+    function updateCarousel() {
+      cards.forEach((card, i) => {
+        card.classList.remove('active');
+        if (i === index) {
+          card.classList.add('active');
+        }
+      });
+    }
 
-  function nextSlide() {
-    index = (index + 1) % cards.length;
+    function nextSlide() {
+      index = (index + 1) % cards.length;
+      updateCarousel();
+    }
+
+    function prevSlide() {
+      index = (index - 1 + cards.length) % cards.length;
+      updateCarousel();
+    }
+
+    // Attach functions to global scope if using inline HTML onclick
+    window.nextSlide = nextSlide;
+    window.prevSlide = prevSlide;
+
     updateCarousel();
-  }
-
-  function prevSlide() {
-    index = (index - 1 + cards.length) % cards.length;
-    updateCarousel();
-  }
-
-  document.addEventListener("DOMContentLoaded", updateCarousel);
+    setInterval(nextSlide, 4000);
+  });
 </script>
